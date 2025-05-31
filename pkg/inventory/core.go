10704038b452b9e0fb7ef50bf9d9987472c54e39
umichan0621 <umichan0621@gmail.com>
2025-05-31 20:58:42 +0800

    1

--- pkg/inventory/core.go
+++ pkg/inventory/core.go
@@ -1,85 +1,17 @@
 package inventory
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"strconv"
+	"steam/pkg/auth"
 )
 
 type Core struct {
-	httpClient *http.Client
-	language   string
+	authCore *auth.Core
+	language string
 }
 
-func (core *Core) Init(httpClient *http.Client) {
-	core.httpClient = httpClient
+func (core *Core) Init(authCore *auth.Core) {
+	core.authCore = authCore
 	core.language = "english"
 }
 
 func (core *Core) SetLanguage(language string) { core.language = language }
-
-func (core *Core) GetTradableItems(steamID string, appID, contextID, startAssetID, count uint64, items *[]InventoryItem) (hasMore bool, lastAssetID uint64, err error) {
-	params := url.Values{
-		"l":     {core.language},
-		"count": {strconv.FormatUint(count, 10)},
-	}
-	if startAssetID != 0 {
-		params.Set("start_assetid", strconv.FormatUint(startAssetID, 10))
-	}
-
-	url := fmt.Sprintf("http://steamcommunity.com/inventory/%s/%d/%d?%s", steamID, appID, contextID, params.Encode())
-	res, err := core.httpClient.Get(url)
-	if err != nil {
-		return false, 0, err
-	}
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return false, 0, err
-	}
-	resp := Response{}
-	err = json.Unmarshal(data, &resp)
-	if err != nil {
-		return false, 0, err
-	}
-
-	descriptions := make(map[string]int)
-	for i, desc := range resp.Descriptions {
-		key := fmt.Sprintf("%d_%d", desc.ClassID, desc.InstanceID)
-		descriptions[key] = i
-	}
-
-	for _, asset := range resp.Assets {
-		var desc *EconItemDesc
-		key := fmt.Sprintf("%d_%d", asset.ClassID, asset.InstanceID)
-		if d, ok := descriptions[key]; ok {
-			desc = resp.Descriptions[d]
-		}
-
-		item := InventoryItem{
-			AppID:      asset.AppID,
-			ContextID:  asset.ContextID,
-			AssetID:    asset.AssetID,
-			ClassID:    asset.ClassID,
-			InstanceID: asset.InstanceID,
-			Amount:     asset.Amount,
-			Desc:       desc,
-		}
-
-		if item.Desc.Tradable != 0 {
-			*items = append(*items, item)
-		}
-	}
-	hasMore = resp.HasMore != 0
-	if !hasMore {
-		return hasMore, 0, nil
-	}
-	lastAssetID, err = strconv.ParseUint(resp.LastAssetID, 10, 64)
-	if err != nil {
-		return hasMore, 0, err
-	}
-	return hasMore, lastAssetID, nil
-}
--- pkg/market/core.go
+++ pkg/market/core.go
@@ -1,11 +1,7 @@
 package market
 
 import (
-	"fmt"
-	"io"
-	"net/http"
 	"steam/pkg/auth"
-	"strings"
 )
 
 type Core struct {
@@ -14,49 +10,4 @@ type Core struct {
 
 func (core *Core) Init(authCore *auth.Core) {
 	core.authCore = authCore
-}
-
-func (core *Core) WalletBalance() (float64, error) {
-	url := "https://store.steampowered.com/account/history/"
-	res, err := core.authCore.HttpClient().Get(url)
-	if err != nil {
-		return 0, err
-	}
-	fmt.Println(core.authCore.HttpClient().Jar)
-
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		return 0, fmt.Errorf("fail to get wallet balance, code: %d", res.StatusCode)
-	}
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return 0, err
-	}
-	x := string(data)
-
-	// q, err := html.Parse(strings.NewReader(x))
-	// if err != nil {
-	// 	return 0, err
-	// }
-	// fmt.Println(x)
-	// findTitle(q)
-
-	index := strings.Index(x, "wallet")
-	fmt.Println(index)
-	if index >= 0 {
-		fmt.Println(data[index:])
-
-	} else {
-		fmt.Println("not found")
-	}
-	// fmt.Println(x)
-	// url = SteamUrl.URL_STEAM_STORE + '/account/history/'
-	// response = self._session.get(url)
-	// response_soup = bs4.BeautifulSoup(response.text, "html.parser")
-	// balance = response_soup.find(id='header_wallet_balance').string
-	// if convert_to_decimal:
-	// 	return parse_price(balance)
-	// else:
-	// 	return balance
-	return 0, nil
 }
