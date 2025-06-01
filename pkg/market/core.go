9cf7759e940bce9653b09e50e990152a90a1aca8
umichan0621 <umichan0621@gmail.com>
2025-06-01 14:12:28 +0800

    add market history order API

--- pkg/market/core.go
+++ pkg/market/core.go
@@ -6,8 +6,12 @@ import (
 
 type Core struct {
 	authCore *auth.Core
+	language string
 }
 
 func (core *Core) Init(authCore *auth.Core) {
 	core.authCore = authCore
+	core.language = "english"
 }
+
+func (core *Core) SetLanguage(language string) { core.language = language }
