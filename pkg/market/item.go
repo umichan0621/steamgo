31883691582af96498c68871f2b07fca67b9f34d
umichan0621 <umichan0621@gmail.com>
2025-06-01 14:04:58 +0800

    1

--- pkg/market/item.go
+++ pkg/market/item.go
@@ -1,36 +1,148 @@
 package market
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
-	"os"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/tidwall/gjson"
+	"golang.org/x/net/html"
 )
 
-func (core *Core) GetItemMarketList() (error, error) {
-	// https://steamcommunity.com/market/listings/730/Glove%20Case
-	res, err := core.authCore.HttpClient().Get("https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20USP-S%20%7C%20Ticket%20to%20Hell%20%28Factory%20New%29")
+type SteamSoldOrders struct {
+	AppID          uint32 `json:"appid"`
+	ContextID      uint64 `json:"contextid,string"`
+	AssetID        uint64 `json:"assetid,string"`
+	ClassID        uint64 `json:"classid,string"`
+	InstanceID     uint64 `json:"instanceid,string"`
+	MarketName     string `json:"market_name"`
+	MarketHashName string `json:"market_hash_name"`
+	SellingPrice   float64
+}
+
+func (core *Core) MarketHistory(appID uint32, contextID uint64) ([]*SteamSoldOrders, error) {
+	params := url.Values{
+		"l":     {core.language},
+		"count": {strconv.FormatUint(20, 10)},
+	}
+
+	url := "https://steamcommunity.com/market/myhistory?" + params.Encode()
+	res, err := core.authCore.HttpClient().Get(url)
 	if err != nil {
 		return nil, err
 	}
-
-	// res, err := core.httpClient.Get("https://steamcommunity.com/market/listings/730/P250%20%7C%20Boreal%20Forest%20%28Field-Tested%29/render/?query=&start=0&count=10")
-	// if err != nil {
-	// 	return nil, err
-	// }
-
 	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("fail to get wallet balance, code: %d", res.StatusCode)
+	}
+
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println(string(data))
-	file, err := os.OpenFile("1.html", os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return nil, nil
+	jsonData := string(data)
+	success := gjson.Get(jsonData, "success").Bool()
+	if !success {
+		return nil, fmt.Errorf("fail to get market history")
 	}
-	defer file.Close()
-	file.WriteString(string(data))
-	return nil, nil
+	assets := gjson.Get(jsonData, "assets")
+	assetsList := assets.Get(strconv.FormatUint(uint64(appID), 10)).Get(strconv.FormatUint(uint64(contextID), 10))
+
+	soldOrdersList := []*SteamSoldOrders{}
+	assetsList.ForEach(func(_, val gjson.Result) bool {
+		soldOrder := &SteamSoldOrders{}
+		err := json.Unmarshal([]byte(val.String()), soldOrder)
+		if err != nil {
+			return false
+		}
+		soldOrdersList = append(soldOrdersList, soldOrder)
+		return true
+	})
+
+	htmlData := gjson.Get(jsonData, "results_html").String()
+	hoversData := gjson.Get(jsonData, "hovers").String()
+	htmlNode, _ := html.Parse(strings.NewReader(htmlData))
+
+	historyRow2AssetID := generateHistoryRow2AssetIDMap(hoversData)
+	historyRow2Price := map[string]float64{}
+	assetID2Price := map[uint64]float64{}
+	generateHistoryRow2PriceMap(htmlNode, &historyRow2Price, "empty")
+
+	for historyRow, assetID := range historyRow2AssetID {
+		price, ok := historyRow2Price[historyRow]
+		if ok {
+			assetID2Price[assetID] = price
+		}
+	}
+
+	for _, soldOrder := range soldOrdersList {
+		assetID := soldOrder.AssetID
+		price, ok := assetID2Price[assetID]
+		if ok {
+			soldOrder.SellingPrice = price
+		}
+	}
+	return soldOrdersList, nil
+}
+func generateHistoryRow2AssetIDMap(hovers string) map[string]uint64 {
+	res := map[string]uint64{}
+	hoversList := strings.Split(hovers, ";")
+	for _, tmp := range hoversList {
+		elements := strings.Split(tmp, ",")
+		if len(elements) <= 4 {
+			continue
+		}
+		historyRow := elements[1]
+		asstIDStr := elements[4]
+		if strings.Contains(historyRow, "_image") {
+			continue
+		}
+		historyRow = strings.ReplaceAll(historyRow, "_name", "")
+		historyRow = strings.ReplaceAll(historyRow, "'", "")
+		historyRow = strings.ReplaceAll(historyRow, " ", "")
+		asstIDStr = strings.ReplaceAll(asstIDStr, "'", "")
+		asstIDStr = strings.ReplaceAll(asstIDStr, " ", "")
+		asstID, err := strconv.ParseUint(asstIDStr, 10, 64)
+		if err == nil {
+			res[historyRow] = asstID
+		}
+	}
+	return res
+}
+
+func generateHistoryRow2PriceMap(n *html.Node, priceMap *map[string]float64, historyRow string) {
+	if n.Type == html.ElementNode {
+		class := ""
+		id := ""
+		for _, attr := range n.Attr {
+			if attr.Key == "class" {
+				class = attr.Val
+			} else if attr.Key == "id" {
+				id = attr.Val
+			}
+			if class == "market_listing_row market_recent_listing_row" {
+				historyRow = id
+			} else if class == "market_listing_price" {
+				priceStr := strings.ReplaceAll(n.FirstChild.Data, "\t", "")
+				index := strings.Index(priceStr, " ")
+				if index >= 0 {
+					priceStr = priceStr[index+1:]
+					price, err := strconv.ParseFloat(priceStr, 64)
+					if err == nil {
+						(*priceMap)[historyRow] = price
+					} else {
+						(*priceMap)[historyRow] = -0.1
+					}
+				}
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		generateHistoryRow2PriceMap(c, priceMap, historyRow)
+	}
 }
